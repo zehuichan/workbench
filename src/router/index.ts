4c985465dc0b914eb40188f5c371a2d15a1f2738
Zehui Chan <jasonchenzehui@gmail.com>
2026-03-09 11:15:26 +0800

    feat: ci

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,12 +6,6 @@ const router = createRouter({
   history: createWebHashHistory(import.meta.env.VITE_BASE),
   // 应该添加到路由的初始路由列表。
   routes,
-  scrollBehavior: (to, _from, savedPosition) => {
-    if (savedPosition) {
-      return savedPosition;
-    }
-    return to.hash ? { behavior: 'smooth', el: to.hash } : { left: 0, top: 0 };
-  },
   // 是否应该禁止尾部斜杠。
   // strict: true,
 });
